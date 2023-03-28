9418eec404c3b031323afcddc234fc48d8ef4ab7
Denys Garagan <denisgaragan2003@gmail.com>
2023-03-28 15:16:43 +0300

    lesson29_first-commit

---
+++ lesson29_JS/script/minimalscript.js
@@ -0,0 +1,12 @@
+let currentUser;
+let currentVisitor;
+let currentClient;
+let userName;
+
+// let userlanguage;
+// let 1userAge;
+// let my-name;
+
+/* Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere quod autem rerum vel quae. Reprehenderit saepe pariatur quidem, id numquam laboriosam impedit alias quos corporis quam? Aut commodi voluptatum quisquam?*/
+
+
---
+++ lesson29_JS/script/normalscript.js
@@ -0,0 +1,14 @@
+"use strict"
+
+alert("Привіт");
+
+let userName = prompt("Як тебе звати ?");
+
+alert("Тебе звати " + userName + "");
+
+let currentUser = prompt('Ваш рік народження?');
+
+let yourAge = 2023 - +currentUser;
+
+alert(`Вам ${yourAge} років`);  
+
