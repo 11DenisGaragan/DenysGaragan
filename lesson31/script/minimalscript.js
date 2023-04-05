5381df34af92d14e6a758843b35d0b00b18b933c
Denys Garagan <denisgaragan2003@gmail.com>
2023-04-05 18:00:57 +0300

    lesson31_first-commit

---
+++ lesson31/script/minimalscript.js
@@ -0,0 +1,60 @@
+// "use strict"
+
+// const userAnswer = prompt(" Скільки Вам років? ");
+// if(!isNaN(userAnswer)) {
+// if (userAnswer <= 11 ) {
+//     alert(' Дитина!');
+// } else if(userAnswer >= 11 && userAnswer <= 17) {
+//     alert('Підліток');
+// } else if (userAnswer >= 18 && userAnswer <= 59) {
+//     alert('Дорослий');
+// } else if (userAnswer <= 60) {
+//     alert('Пенціонер');
+// }
+// } else {
+//     alert('Ви ввели не коректні данні');
+// }
+
+// let number = prompt(" Ведіть число від 0 до 9 ");
+// if(!isNaN(number) && number <= 9) {  
+//     if (number == 0) {
+//         alert(')');
+//     } else if (number == 1) {
+//         alert('!');
+//     } else if (number == 2) {
+//         alert('@');
+//     } else if (number == 3) {
+//         alert('#');ß
+//     } else if (number == 4) {
+//         alert('$');
+//     } else if (number == 5) {
+//         alert('%');
+//     } else if (number == 6) {
+//         alert('^');
+//     } else if (number == 7) {
+//         alert('&');
+//     } else if (number == 8) {
+//         alert('*');
+//     } else if (number == 9) {
+//         alert('(');
+//     }
+// } else {
+//     alert('Ви ввели не коректни данні');
+// }
+
+// let a = +prompt("Введите число","");
+// let b = Number(prompt("Введите число",""));
+// let sum = null;
+    
+// while (a<b)
+// {
+// a++;
+          
+// if(a<b) 
+// {
+//    sum+=a;
+// }
+// }    
+// document.write(sum);
+
+
---
+++ lesson31/script/normalscript.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let result = prompt("Введите значение");
+let stringArr = result.split("").reverse().join("");
+  
+if (result == stringArr) {
+  console.log("Данное значение является палиндромом");
+} else {
+  console.log("Данное значение не является палиндромом");
+}
