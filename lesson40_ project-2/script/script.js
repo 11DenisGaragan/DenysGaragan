7b838d289e3df129fffb37f13a4b12f2be915099
Denys Garagan <denisgaragan2003@gmail.com>
2023-04-27 20:31:18 +0300

    added-layut

--- lesson40_ project-2/script/script.js
+++ lesson40_ project-2/script/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     pagination: {
       el: ".swiper-pagination",
@@ -10,12 +10,35 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
   });
 
-  var swiper2 = new Swiper(".mySwiper2", {
+  const swiper2 = new Swiper(".mySwiper2", {
    slidesPerView: 3,
    spaceBetween: 30,
+   navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
    pagination: {
     el: ".swiper-pagination",
     clickable: true,
    },
    loop: true,
   });
+
+
+
+
+
+  input.onblur = function() {
+    if (!input.value.includes('@')) { // це не електронна адреса
+      input.classList.add('invalid');
+      error.innerHTML = 'Будь ласка, введіть правильну електронну адресу.'
+    }
+  };
+  
+  input.onfocus = function() {
+    if (this.classList.contains('invalid')) {
+      // видалити індикатор помилки, тому що користувач хоче ввести дані заново
+      this.classList.remove('invalid');
+      error.innerHTML = "";
+    }
+  };
