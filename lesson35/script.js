00e48e5f6c81b563850470e64c28c907e6d047c6
Denys Garagan <denisgaragan2003@gmail.com>
2023-05-10 21:23:40 +0300

    lesson35_first-commit

---
+++ lesson35/script.js
@@ -0,0 +1,73 @@
+let playList = [
+    {
+     author: "LED ZEPPELIN",
+    
+     song:"STAIRWAY TO HEAVEN"
+    },
+    {
+     author: "QUEEN",
+    
+     song:"BOHEMIAN RHAPSODY"
+    },
+    {
+     author: "LYNYRD SKYNYRD",
+    
+     song:"FREE BIRD"
+    },
+    {
+     author: "DEEP PURPLE",
+    
+     song:"SMOKE ON THE WATER"
+    },
+    {
+     author: "JIMI HENDRIX",
+    
+     song:"ALL ALONG THE WATCHTOWER"
+    },
+    {
+     author: "AC/DC",
+    
+     song:"BACK IN BLACK"
+    },
+    {
+     author: "QUEEN",
+    
+     song:"WE WILL ROCK YOU"
+    },
+    {
+     author: "METALLICA",
+    
+     song:"ENTER SANDMAN"
+    }
+];
+
+function createSongElement(data) {
+    const { author, song} = data;
+
+    const box = document.createElement('div');
+    box.className = 'song';
+
+    const logo = document.createElement('div');
+    logo.className = 'song__logo';
+
+    const title = document.createElement('h3');
+    title.className = 'song__title';
+    title.innerText = song;
+    title.title = song;
+
+    const subtitle = document.createElement('h4');
+    subtitle.className = 'song__subtitle';
+    subtitle.innerText =  author;
+
+    box.append(logo, title, subtitle);
+
+    return box;
+}
+
+const root = document.getElementById('songs');
+
+playList.forEach((data) => {
+    root.append(
+        createSongElement(data)
+    );
+});
